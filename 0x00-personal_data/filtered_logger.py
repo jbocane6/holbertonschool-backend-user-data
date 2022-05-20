55cc47f90bbb9fa439fe42f5cc5ae9e33667ac45
Juan Bocanegra <jbocane6@gmail.com>
2022-05-19 21:50:15 -0500

    [UPDATE] task filtered_logger v.1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+"""
+"""
+from typing import List
+import re
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """returns the log message obfuscated"""
+
+    for i in fields:
+        message = re.sub(i + "=.*?" + separator,
+                         i + "=" + redaction + separator,
+                         message)
+    return message
