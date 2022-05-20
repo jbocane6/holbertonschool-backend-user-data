12bb9c66bb2a11fdacaf2ccc2b4efa57534abeab
Juan Bocanegra <jbocane6@gmail.com>
2022-05-19 22:33:32 -0500

    [UPDATE] task 6 v.1

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -11,3 +11,11 @@ def hash_password(password: str) -> bytes:
     and returns a salted, hashed password, which is a byte string.
     """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Function that expects 2 arguments and returns a boolean
+    to validate that the provided password matches the hashed password.
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
