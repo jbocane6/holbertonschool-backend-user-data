1432d8883fa1d08d2ff120eaab99302a2c0d019f
Juan Bocanegra <jbocane6@gmail.com>
2022-05-19 22:30:20 -0500

    [UPDATE] task 5 v.1

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+"""
+User passwords should NEVER be stored in plain text in a database.
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    function that expects one string argument name password
+    and returns a salted, hashed password, which is a byte string.
+    """
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
