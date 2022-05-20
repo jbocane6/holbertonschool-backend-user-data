788c74dbe8392707100112187cb3c29de1b66916
Juan Bocanegra <jbocane6@gmail.com>
2022-05-19 21:54:05 -0500

    [UPDATE] task 0 v.1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """
+Write a function called filter_datum that returns the log message obfuscated:
 """
 from typing import List
 import re
@@ -7,10 +8,8 @@ import re
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
-    """returns the log message obfuscated"""
-
+    """Returns the log message obfuscated"""
     for i in fields:
-        message = re.sub(i + "=.*?" + separator,
-                         i + "=" + redaction + separator,
-                         message)
-    return message
+        response = re.sub(i + "=.*?" + separator,
+                         i + "=" + redaction + separator, message)
+    return response
