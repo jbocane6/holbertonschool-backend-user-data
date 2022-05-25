64030c23bba345d75600effb6da1af76e622a32a
Juan Bocanegra <jbocane6@gmail.com>
2022-05-24 22:38:44 -0500

    [UPDATE] task 4 v.1

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,14 +7,16 @@ from typing import List, TypeVar
 
 
 class Auth:
-    """
-    Auth class
-    """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
         require_auth function
         """
-        return False
+        if path is None or excluded_paths is None or not excluded_paths:
+            return True
+        if path in excluded_paths or path[-1] != '/' and path + '/'\
+                in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
