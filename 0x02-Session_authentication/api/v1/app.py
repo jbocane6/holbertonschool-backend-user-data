1490ec51238b0a74d16b8f8bf5b86544356f41d6
Juan Bocanegra <jbocane6@gmail.com>
2022-05-26 19:21:02 -0500

    [UPDATE] task 5 v.1

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -17,6 +17,9 @@ auth = None
 if getenv('AUTH_TYPE') == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+elif getenv('AUTH_TYPE') == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 else:
     from api.v1.auth.auth import Auth
     auth = Auth()
@@ -31,11 +34,13 @@ def before_request() -> None:
     excluded_paths = [
             '/api/v1/status/',
             '/api/v1/unauthorized/',
-            '/api/v1/forbidden/'
+            '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/'
             ]
     if auth:
         if auth.require_auth(request.path, excluded_paths):
-            if not auth.authorization_header(request):
+            if not auth.authorization_header(request)\
+                    and not auth.session_cookie(request):
                 abort(401)
             if not auth.current_user(request):
                 abort(403)
