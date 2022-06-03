3e8b9b02cd04838c1196199ae73ae5c13aecb122
Juan Bocanegra <jbocane6@gmail.com>
2022-06-02 21:39:38 -0500

    [UPDATE] task 1 v.1

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""
+DB module
+"""
+from curses.ascii import US
+from turtle import st
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import Base, User
+
+
+class DB:
+    """
+    DB class
+    """
+
+    def __init__(self) -> None:
+        """
+        Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """
+        Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """
+        Implement the add_user method, which should save the user
+        to the database.
+        Arguments:
+            email: A non-nullable string.
+            hashed_password: A non-nullable string.
+        Returns:
+            User object.
+        """
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
+        return new_user
