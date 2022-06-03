a5003996c2468b459563f9cbe31d26816b243b0c
Juan Bocanegra <jbocane6@gmail.com>
2022-06-02 22:13:02 -0500

    [UPDATE] task 0 v.3

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -19,11 +19,3 @@ class User(Base):
     hashed_password = Column(String(250))
     session_id = Column(String(250))
     reset_token = Column(String(250))
-
-    def __repr__(self):
-        """
-        Returns the object representation in string format.
-        """
-        return "<User(email='%s', hashed_password='%s', session_id='%s',\
-                      reset_token='%s')>" % (self.email, self.hashed_password,
-                                             self.session_id, self.reset_token)
