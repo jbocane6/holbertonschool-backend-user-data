dc1b2dfc0a6e589c269c9ce37b4f57037a5661f9
Juan Bocanegra <jbocane6@gmail.com>
2022-06-02 22:45:36 -0500

    [UPDATE] task 6 v.1

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Flask app
+"""
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello() -> str:
+    """
+    Home route
+    Returns:
+        str: Home page payload
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
