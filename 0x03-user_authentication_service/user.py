8625323db14ff33baf87631541b7e9ce7b8636b8
Juan Bocanegra <jbocane6@gmail.com>
2022-06-02 21:14:45 -0500

    [UPDATE] task 0 v.2 missed comments

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-
+"""
+SQLAlchemy model named User for a database table named users.
+"""
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
@@ -8,6 +10,9 @@ Base = declarative_base()
 
 
 class User(Base):
+    """
+    Class User
+    """
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     email = Column(String(250))
@@ -16,6 +21,9 @@ class User(Base):
     reset_token = Column(String(250))
 
     def __repr__(self):
+        """
+        Returns the object representation in string format.
+        """
         return "<User(email='%s', hashed_password='%s', session_id='%s',\
                       reset_token='%s')>" % (self.email, self.hashed_password,
                                              self.session_id, self.reset_token)
